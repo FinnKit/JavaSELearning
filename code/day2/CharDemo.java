824d06d421b197377fa10ba2b35e82368efc9fd7
Finn <gcm2021@126.com>
2021-06-21 11:21:28 +0800

    Update the Day2 Note

---
+++ code/day2/CharDemo.java
@@ -0,0 +1,12 @@
+/*
+	²âÊÔ×Ö·û´®ÔËËã
+*/
+
+class CharDemo{
+	public static void main(String[] Args){
+		System.out.println("Hello" + 'a' + 1);
+		System.out.println('a' + 1 + "Hello");
+		System.out.println(5 + 5 + "= 5 + 5");
+		System.out.println("5 + 5 = " + 5 + 5);
+	}
+}
---
+++ code/day2/DataTypeDemo1.java
@@ -0,0 +1,10 @@
+/*
+	测试强制数据转换
+*/
+
+class DataTypeDemo1{
+	public static void main(String[] Args){
+		byte a = (byte)130;
+		System.out.println(a);
+	}
+}
